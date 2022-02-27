487f9848ff60e5a4a0b9ba81a4b9179124c3ae9f
LimeTree <bum9516@gmail.com>
2022-02-27 19:59:08 +0900

    binary tree traversal

--- Data Structure/binary_tree_array.cpp
+++ Data Structure/binary_tree_array.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#define SIZE 5
+#define SIZE 15
 
 typedef struct TreeNode{
 	int data;
@@ -60,7 +60,7 @@ void printFor() {
 //전위 순회
 void preorder(int parentNode) {
 	if (parentNode < SIZE) {
-		printf_s("%d ", tree[parentNode].data);
+		printf_s(" %d ->", tree[parentNode].data);
 		preorder(parentNode * 2 + 1);
 		preorder(parentNode * 2 + 2);
 	}
@@ -70,7 +70,7 @@ void preorder(int parentNode) {
 void inorder(int parentNode) {
 	if (parentNode < SIZE) {
 		inorder(parentNode * 2 + 1);
-		printf_s("%d ", tree[parentNode].data);
+		printf_s(" %d ->", tree[parentNode].data);
 		inorder(parentNode * 2 + 2);
 	}
 }
@@ -80,9 +80,41 @@ void postorder(int parentNode) {
 	if (parentNode < SIZE) {
 		postorder(parentNode * 2 + 1);
 		postorder(parentNode * 2 + 2);
-		printf_s("%d ", tree[parentNode].data);
+		printf_s(" %d ->", tree[parentNode].data);
 	}
 }
+
+// 트리로 출력하려다 망한 함수1
+void test(int spaceCount, int space, int *index, bool* elementprint) {
+	if (spaceCount%space == 0) {
+		if (*elementprint) {
+			printf_s("%-4d", tree[(*index)++].data);
+			*elementprint = false;
+		}
+		else {
+			printf_s("s");
+			*elementprint = true;
+		}
+	}
+	else
+		printf_s("s");
+}
+
+// 트리로 출력하려다 망한 함수2
+void test2() {
+	bool elementPrint = true;
+	int index = 0;
+	int space = (SIZE + 1) / 2;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 1; j < SIZE+1; j++)
+			if(index <SIZE)
+				test(j, space, &index, &elementPrint);
+		printf_s("\n");
+		elementPrint = true;
+		space /= 2;
+	}
+}
+
 
 int main() {
 
@@ -90,5 +122,5 @@ int main() {
 	printf_s("preorder  : "); preorder(0); printf_s("\n");
 	printf_s("inorder   : "); inorder(0); printf_s("\n");
 	printf_s("postorder : "); postorder(0); printf_s("\n");
-	//printFor();
+	printFor();
 }
